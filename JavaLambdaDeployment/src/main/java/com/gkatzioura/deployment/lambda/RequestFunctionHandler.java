07bde97df61b12866e0c1f24ea5eeb8dbac635bf
gkatzioura <gkatzioura@gmail.com>
2016-10-17 09:25:07 +0300

    Added lambda function

--- JavaLambdaDeployment/src/main/java/com/gkatzioura/deployment/lambda/RequestFunctionHandler.java
+++ JavaLambdaDeployment/src/main/java/com/gkatzioura/deployment/lambda/RequestFunctionHandler.java
@@ -3,6 +3,7 @@ package com.gkatzioura.deployment.lambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -11,13 +12,28 @@ import java.util.logging.Logger;
  */
 public class RequestFunctionHandler implements RequestHandler<Map<String,String>,String> {
 
+    private static final String NUMERATOR_KEY = "numerator";
+    private static final String DENOMINATOR_KEY = "denominator";
+
     private static final Logger LOGGER = Logger.getLogger(RequestFunctionHandler.class.getName());
 
     public String handleRequest(Map <String,String> values, Context context) {
 
         LOGGER.info("Handling request");
 
-        return "You invoked a lambda function";
+        if(!values.containsKey(NUMERATOR_KEY)||!values.containsKey(DENOMINATOR_KEY)) {
+            return "You need both numberator and denominator";
+        }
+
+        new BigDecimal(23.3);
+
+        try {
+            BigDecimal numerator = new BigDecimal(values.get(NUMERATOR_KEY));
+            BigDecimal denominator= new BigDecimal(values.get(DENOMINATOR_KEY));
+            return  numerator.divide(denominator).toString();
+        } catch (Exception e) {
+            return "Please provide valid values";
+        }
     }
 
 }
